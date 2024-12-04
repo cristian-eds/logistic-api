06b78cafb3ecc2d7623bb9907863241d731748fb
Cristian <cristian.santos0308@gmail.com>
2024-12-04 17:59:40 -0300

    Get client by id

--- src/main/java/com/cristian_eds/logistica_api/controller/ClientController.java
+++ src/main/java/com/cristian_eds/logistica_api/controller/ClientController.java
@@ -2,29 +2,45 @@ package com.cristian_eds.logistica_api.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristian_eds.logistica_api.domain.model.Client;
 import com.cristian_eds.logistica_api.repository.ClientRepository;
 
+
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 	
 	@Autowired
 	private ClientRepository repository;
 	
-	@GetMapping("/clients")
+	@GetMapping("/test")
 	public List<Client> listClients() {
 		var client1 = new Client(1l,"joao","joao@emial.com","13213232");
 		var clients = Arrays.asList(client1);
 		return clients;
 	}
 	
-	@GetMapping
+	@GetMapping()
 	public List<Client> getClients() {
 		return repository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> getClientById(@PathVariable Long id) {
+		Optional<Client> client = repository.findById(id);
+		if(client.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(client.get());
+	}
+
 }
--- src/main/java/com/cristian_eds/logistica_api/repository/ClientRepository.java
+++ src/main/java/com/cristian_eds/logistica_api/repository/ClientRepository.java
@@ -1,9 +1,15 @@
 package com.cristian_eds.logistica_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cristian_eds.logistica_api.domain.model.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
+	
+	List<Client> findByName(String name);
 
 }
